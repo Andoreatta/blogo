c6ccc385d757eb4ed5a6b849b12edd8474501e6c
Lucas "Piekha" Andreatta <andreattovysk@tutanota.com>
2023-02-16 16:31:21 -0300

    new SQL table and some random inserts

---
+++ api/sql/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO users (username, email, passwd)
+    VALUES
+        ("user10", "usermail10@mail.com", "pasdsfgsehr"),
+        ("user12", "usermail12@mail.com", "paasasdasddsfgsehr"),
+        ("user14", "usermail14@mail.com", "pasdsfgsehadadsdasr"),
+        ("user16", "usermail16@mail.com", "pasdsfgsdgdfehr");
+
+INSERT INTO followers (userId, followerId)
+    VALUES
+        (1,2),
+        (3,1),
+        (1,3);
